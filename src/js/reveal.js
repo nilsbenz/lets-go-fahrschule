0aa3b174ff5c1f96461144d6bc97e6678e82fe4e
nilsbenz <nils.benz@icloud.com>
2020-06-01 13:58:55 +0200

    feat: reveal on scroll

---
+++ src/js/reveal.js
@@ -0,0 +1,9 @@
+ScrollReveal({ reset: true }).reveal('.card', { scale: 0.5, duration: 800 });
+ScrollReveal({ reset: true }).reveal('.heading-sign', {
+  scale: 0.5,
+  duration: 800,
+});
+ScrollReveal({ reset: false }).reveal('.about-image', {
+  scale: 0.5,
+  duration: 800,
+});
