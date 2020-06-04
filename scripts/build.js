ff1f3c0a7972b9a642ec2e89961a035b51e9e617
nilsbenz <nils.benz@icloud.com>
2020-06-04 14:34:37 +0200

    feat: init npm

---
+++ scripts/build.js
@@ -0,0 +1,56 @@
+const ncp = require('ncp').ncp;
+const rimraf = require('rimraf');
+const minify = require('@node-minify/core');
+const cleanCSS = require('@node-minify/clean-css');
+const babelMinify = require('@node-minify/babel-minify');
+const htmlMinifier = require('@node-minify/html-minifier');
+
+ncp.limit = 16;
+
+rimraf('./build', () => {
+  ncp('./src/icons', './build', (err) => {
+    if (err) {
+      console.error(err);
+    }
+    ncp('./src', './build', (err) => {
+      if (err) {
+        console.error(err);
+      }
+      rimraf('./build/icons', (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      minify({
+        compressor: babelMinify,
+        input: './build/js/*.js',
+        output: './build/js/$1.js',
+        callback: function (err) {
+          if (err) {
+            console.error(err);
+          }
+        },
+      });
+      minify({
+        compressor: cleanCSS,
+        input: './build/css/*.css',
+        output: './build/css/$1.css',
+        callback: function (err) {
+          if (err) {
+            console.error(err);
+          }
+        },
+      });
+      minify({
+        compressor: htmlMinifier,
+        input: './build/*.html',
+        output: './build/$1.html',
+        callback: function (err) {
+          if (err) {
+            console.error(err);
+          }
+        },
+      });
+    });
+  });
+});
